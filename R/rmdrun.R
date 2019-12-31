1f907e84fd2a351536f5a957625071e6c6189c49
Karandeep Singh <kdpsingh@users.noreply.github.com>
2019-12-30 20:56:53 -0500

    Fixed rmdrun arguments

--- R/rmdrun.R
+++ R/rmdrun.R
@@ -9,7 +9,7 @@ rmdrun <- function(text, ...){
     stop('For rmdrun(), the file must have a .Rmd extension.')
   }
   writeLines(text, con=paste0('input',filetype))
-    rmarkdown::run(input=paste0('input',filetype), output_file='output.html', output_format='html_document', ...);
+    rmarkdown::run(file=paste0('input',filetype), ...);
     invisible();
 }
 
