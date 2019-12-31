946a889976127eb6282ace40e1b8f001eb9495d8
Karandeep Singh <kdpsingh@users.noreply.github.com>
2019-12-30 21:04:10 -0500

    removed ... from rmdrun

--- R/rmdrun.R
+++ R/rmdrun.R
@@ -2,14 +2,14 @@
 ##
 
 #' @export
-rmdrun <- function(text, ...){
+rmdrun <- function(text){
   if (grepl('```', text)) {
     filetype = '.Rmd'
   } else {
     stop('For rmdrun(), the file must have a .Rmd extension.')
   }
   writeLines(text, con=paste0('input',filetype))
-    rmarkdown::run(file=paste0('input',filetype), ...);
+    rmarkdown::run(file=paste0('input',filetype));
     invisible();
 }
 
