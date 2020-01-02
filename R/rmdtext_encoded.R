08e0acf88ea48fb9f2e5cd1d414cc1dc415b6478
Karandeep Singh <kdpsingh@users.noreply.github.com>
2020-01-02 02:12:31 -0500

    removed fixed output format and recognize ^--- as Rmd

--- R/rmdtext_encoded.R
+++ R/rmdtext_encoded.R
@@ -5,13 +5,13 @@
 rmdtext_encoded <- function(text){
 
   text = URLdecode(text)
-  if (grepl('```', text)) {
+  if (grepl('```', text) | grepl('^---', text)) {
     filetype = '.Rmd'
   } else {
     filetype = '.R'
   }
-  writeLines(text, con=paste0('input',filetype))
-    rmarkdown::render(input=paste0('input',filetype), output_file='output.html', output_format='html_document');
+  writeLines(text, con=paste0('output',filetype))
+    rmarkdown::render(input=paste0('output',filetype));
     invisible();
 }
 
