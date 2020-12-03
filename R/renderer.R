b2aa8b1bca95450ec1d98a90c74893842e7abea8
Karandeep Singh <kdpsingh@users.noreply.github.com>
2020-12-03 12:54:56 -0500

    Convert &gt; to > to fix pipes (and other html chars).

--- R/renderer.R
+++ R/renderer.R
@@ -2,9 +2,15 @@
 renderer <- function(text, ...) {
 
   text = URLdecode(text)
-  if (!grepl('```', text) & !grepl('^---', text)) {
+  if (!grepl('```', text) & !grepl('^\\s*?---', text)) {
     text = paste0('```{r}\n',text,'\n```')
   }
+  
+  unescape_html <- function(str){
+    xml2::xml_text(xml2::read_html(paste0("<x>", str, "</x>")))
+  }
+  
+  text = unescape_html(text)
   
   writeLines(text, con='output.Rmd')
   rmarkdown::render(input='output.Rmd', ...)
